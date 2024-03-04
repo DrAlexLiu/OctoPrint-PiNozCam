49beaa7740ad5e36052c710610e0403546ab87aa
Dr.AlexLiu <69814118+DrAlexLiu@users.noreply.github.com>
2024-03-03 20:53:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ plugin_author = "DrAlexLiu"
 plugin_author_email = "xc.liu@mail.utoronto.ca"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/DrAlexLiu/PiNozCAM"
+plugin_url = "https://github.com/DrAlexLiu/OctoPrint-PiNozCAM"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
